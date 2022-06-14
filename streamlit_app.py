d92e049bbffe71aa3b5bd8e07d65d67e793698d2
parkitdev <parkitdevs@gmail.com>
2022-06-14 23:15:47 +0530

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 #created forst python file
 import streamlit
 streamlit.title("Vishudh Bhog!")
+streamlit.header("Breakfast menu!")
+streamlit.text("Sabudana Tikki!")
+streamlit.text("Singhare ka parathan!")
+streamlit.text("Vaishnav thali!")
