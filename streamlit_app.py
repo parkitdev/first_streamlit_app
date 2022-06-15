7905d12c0463583e6c7e8eaef1bd4d0c5957a753
parkitdev <parkitdevs@gmail.com>
2022-06-15 22:29:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,5 @@ streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}
 streamlit.text("Singhare ka parathan!")
 streamlit.text("Vaishnav thali!")
 my_fruit_df=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.multiselect('Choose the fruit to make smoothies',my_fruit_df.index)
 streamlit.dataframe(my_fruit_df)
