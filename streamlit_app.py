8196e9d6e3deb046df5e828aae5e53f896844c08
parkitdev <parkitdevs@gmail.com>
2022-06-15 21:03:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 #from emoji import emojize
 streamlit.title("Vishudh Bhog!")
 streamlit.header("Breakfast menu!")
-#streamlit.text(emojize(":chicken:"))
+streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 streamlit.text("Singhare ka parathan!")
 streamlit.text("Vaishnav thali!")
