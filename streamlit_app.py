ef4e9c8cf7f5a50ab9be81d585d055fc4ef4d4ec
parkitdev <parkitdevs@gmail.com>
2022-06-15 20:32:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 #created forst python file
-import emoji
 import streamlit
 streamlit.title("Vishudh Bhog!")
 streamlit.header("Breakfast menu!")
-streamlit.text(":chicken Sabudana Tikki!")
+streamlit.text(":chicken: Sabudana Tikki!")
 streamlit.text("Singhare ka parathan!")
 streamlit.text("Vaishnav thali!")
