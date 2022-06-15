da2d1357c2a28ba5d98d6e73665a5afdfce0957c
parkitdev <parkitdevs@gmail.com>
2022-06-15 22:24:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 #created forst python file
 import streamlit
+import pandas as pd
 #from emoji import emojize
 streamlit.title("Vishudh Bhog!")
 streamlit.header("Breakfast menu!")
 streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 streamlit.text("Singhare ka parathan!")
 streamlit.text("Vaishnav thali!")
+my_fruit_df=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_df)
